86d37e0feaea12b2b54528a46b30006b24754ce3
Khang Dang <113054344+kqdang01@users.noreply.github.com>
2025-05-23 06:07:41 -0500

    add ui to rps game

--- javascript.js
+++ javascript.js
@@ -3,15 +3,40 @@ let cpuScore = 0;
 
 function play()
 {
-    let playerChoice = getHumanChoice();
+    let playerChoice = this.classList.toString();
     let computerChoice = getComputerChoice();
+
+    const playerImage = document.querySelector(".playerPic");
+    const cpuImage = document.querySelector(".cpuPic");
+
+    playerImage.height = 300;
+    cpuImage.height = 300;
+
+    const playerHealth = document.querySelector(".playerHealth");
+    const cpuHealth = document.querySelector(".cpuHealth");
+    const winningtext = document.querySelector(".winner");
+
+    switch(computerChoice)
+    {
+        case "rock":
+            cpuImage.src = "./images/rock.png";
+            break;
+        case "paper":
+            cpuImage.src = "./images/paper.png";
+            break;
+        case "scissors":
+            cpuImage.src = "./images/scissors.png";
+            break;
+    }
 
     switch (playerChoice)
     {
         case "rock":
+            playerImage.src = "./images/rock.png";
             if (computerChoice === "scissors")
             {
                 console.log("cpu chose scissors, you win!");
+                cpuHealth.querySelector("div").remove();
                 playerScore++;
             }
             else if (computerChoice === "rock")
@@ -21,14 +46,17 @@ function play()
             else
             {
                 console.log("cpu chose paper, you lose!");
+                playerHealth.querySelector("div").remove();
                 cpuScore++;
             }
             break;
             
         case "paper":
+            playerImage.src = "./images/paper.png";
             if (computerChoice === "rock")
             {
                 console.log("cpu chose rock, you win!");
+                cpuHealth.querySelector("div").remove();
                 playerScore++;
             }
             else if (computerChoice === "paper")
@@ -38,14 +66,17 @@ function play()
             else
             {
                 console.log("cpu chose scissors, you lose!");
+                playerHealth.querySelector("div").remove();
                 cpuScore++;
             }
             break;
 
         case "scissors":
+            playerImage.src = "./images/scissors.png";
             if (computerChoice === "paper")
             {
                 console.log("cpu chose paper, you win!");
+                cpuHealth.querySelector("div").remove();
                 playerScore++;
             }
             else if (computerChoice === "scissors")
@@ -55,17 +86,23 @@ function play()
             else
             {
                 console.log("cpu chose rock, you lose!");
+                playerHealth.querySelector("div").remove();
                 cpuScore++;
             }
             break;
             default:
                 console.log("invalid option, please try again.")
-                play();
-                break;
     }
-    console.log("score is now: ");
-    console.log("you: " + playerScore);
-    console.log("cpu: " + cpuScore);
+    if (playerScore === 5)
+    {
+        winningtext.textContent = "I knew you'd win!!!";
+        gameEnd();
+    }
+    if (cpuScore === 5)
+    {
+        winningtext.textContent = "You're a loser.";
+        gameEnd();
+    }
 }
 
 function getComputerChoice()
@@ -75,21 +112,26 @@ function getComputerChoice()
     return array[random];
 }
 
-function getHumanChoice()
+function gameEnd()
 {
-    let answer = prompt("Choose your option: (rock, paper, scissors)");
-    answer = answer.toLowerCase();
-    return answer;
+    const choicesContainer = document.querySelector(".choicesContainer");
+    const choiceButtons = choicesContainer.querySelectorAll("button");
+
+    choiceButtons.forEach(button => button.remove());
+
+    document.querySelector(".player").remove();
+    document.querySelector(".cpu").remove();
+
+    const retryButton = document.createElement("button");
+    retryButton.className = "retry";
+    retryButton.textContent = "Retry";
+    retryButton.onclick = () => location.reload();
+    choicesContainer.appendChild(retryButton);
 }
 
-function playGame(num = 5)
-{
-    playerScore = 0;
-    cpuScore = 0;
-    for (let i = 0; i < num; i++)
-    {
-        play();
-    }
-}
-
-playGame();
+const choicesContainer = document.querySelector(".choicesContainer");
+const buttons = choicesContainer.querySelectorAll("button");
+buttons.forEach(button => {
+    console.log("found a button!")
+    button.addEventListener("click", play);
+});
